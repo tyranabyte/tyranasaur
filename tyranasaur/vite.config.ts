0756dcee9f0644929da55e81d5222225f898d56e
adenobyte <adenobyte@outlook.com>
2024-11-03 12:52:36 +0000

    Update Vite config

--- tyranasaur/vite.config.ts
+++ tyranasaur/vite.config.ts
@@ -3,6 +3,7 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/tyranasaur/', // Set the base path for GitHub Pages
     plugins: [plugin()],
     server: {
         port: 55168,
